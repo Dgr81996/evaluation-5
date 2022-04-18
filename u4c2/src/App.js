8ed2968f9cd86c7483eb29ead2baf1b397e61011
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-04-18 21:42:27 +0530

    Add files via upload

---
+++ u4c2/src/App.js
@@ -0,0 +1,104 @@
+
+
+const container={ display:'grid',
+gridTemplateColumns: '25% 25% 25% 25%',
+
+marginTop: '5%',
+marginLeft: '5%',
+marginright:'5%',
+
+gridGap:'20px'}
+
+
+const nav={width:'100%',height:'40px',border:'3px solid black',display:'flex',justifyContent: 'space-around'}
+
+
+
+
+function App() {
+  return (
+    <div>
+     <div style={nav}>Groceries</div>
+
+<div style={container}>
+
+
+
+<div>
+<img source={{uri:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Fbeautiful-luxury-home-exterior-at-twilight-picture-id1026205392%3Fk%3D20%26m%3D1026205392%26s%3D612x612%26w%3D0%26h%3DlYFMV5cOuQQpddmwsE5QLBCyhgWQ1OI46i_dalro9OE%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fhouse&tbnid=LOAHUOPeDQPEbM&vet=12ahUKEwiDp_-y_Z33AhUpzKACHYzDAMAQMygGegUIARDuAQ..i&docid=g9WlojmsydTXDM&w=612&h=408&q=house%20images&client=firefox-b-d&ved=2ahUKEwiDp_-y_Z33AhUpzKACHYzDAMAQMygGegUIARDuAQ"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+<div>
+<img source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlvguNopCtALKDHX8GjqLvIRhE2OH0jg3M6g&usqp=CAU"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+<div>
+<img source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMBobxYJbgGfviYGqPhqGM2kWdhk_c-FoZaw&usqp=CAU"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+
+
+<div>
+<img source={{uri:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Fbeautiful-luxury-home-exterior-at-twilight-picture-id1026205392%3Fk%3D20%26m%3D1026205392%26s%3D612x612%26w%3D0%26h%3DlYFMV5cOuQQpddmwsE5QLBCyhgWQ1OI46i_dalro9OE%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fhouse&tbnid=LOAHUOPeDQPEbM&vet=12ahUKEwiDp_-y_Z33AhUpzKACHYzDAMAQMygGegUIARDuAQ..i&docid=g9WlojmsydTXDM&w=612&h=408&q=house%20images&client=firefox-b-d&ved=2ahUKEwiDp_-y_Z33AhUpzKACHYzDAMAQMygGegUIARDuAQ"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+<div>
+<img source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlvguNopCtALKDHX8GjqLvIRhE2OH0jg3M6g&usqp=CAU"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+<div>
+<img source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMBobxYJbgGfviYGqPhqGM2kWdhk_c-FoZaw&usqp=CAU"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+<div>
+<img source={{uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMBobxYJbgGfviYGqPhqGM2kWdhk_c-FoZaw&usqp=CAU"}}/>
+<p>Name:Deepak</p>
+<p> Owner Name:Deepak</p>
+<p>Address:Delhi </p>
+<p>Area code:9633 </p>
+<button>View</button>
+</div>
+
+
+
+
+
+
+
+
+</div>
+    </div>
+  );
+}
+
+export default App;
